473b8d7d0e68d69accb8d547247fc2709585f2f2
mylesstokowski <35432444+mylesstokowski@users.noreply.github.com>
2020-06-29 16:15:42 -0400

    reorganized into functions, added summarizing

--- raster2table.py
+++ raster2table.py
@@ -1,64 +1,40 @@
-# Myles Stokowski
-# 6.22.20
-#
-# script to generate a table of average SAR amplitude within each 
-# unique road segment identified by OBJECTID
+"""
+Myles Stokowski
+6.22.20
 
-import sys
+Functionality: Takes in a 'mask' and 'data' raster and outputs a  DataFrame 
+with the values from both rasters for each pixel where the neither raster is 
+'nodata'. Then, provides options for how to summarize the 'data' raster 
+for each value/group in the 'mask' raster.
+
+Middsense use case: calculate average SAR amplitude for the ~100 pixels under
+each segment of road, uniquely identified by the road OBJECTID
+data raster = a SAR amplitude raster
+mask raster = an OBJECTID road raster 
+
+Documentation/variable names are largely generalized, but sometimes refer directly
+to the Middsense data
+"""
+
 import argparse
 import numpy as np
 import pandas as pd
+import time
 try: 
     from osgeo import gdal
-    from osgeo.gdalnumeric import *
+    from osgeo.gdalnumeric import * 
     from osgeo.gdalconst import *
 except ImportError:
     sys.exit('ERROR: cannot find GDAL/OGR modules')
 
-# this chunk (before defining raster_group_means) is just for
-# testing to pass the files without running from command line
-# so I can run interactively
-maskpath = '/home/myles/middsense/OID Road Rasters/'
-datapath = '/home/myles/middsense/data_from_drive/SAR_Amplitude/'
-oid = pd.DataFrame()
-oid_list = [oid]
-for data in os.listdir('/home/myles/middsense/data_from_drive/SAR_Amplitude/'):
-    for mask in os.listdir('/home/myles/middsense/OID Road Rasters/'):
-        if mask[-7:] == 'All.tif':
-            #print(datapath + data, maskpath + mask)
-            #pd.concat(oid_list, raster_group_means(datapath + data, maskpath + mask, ''))
-            oid_list.append(raster_group_means(datapath + data, maskpath + mask, ''))
-oid_concat = pd.concat(oid_list)
-
-# which OID values are missing?
-unique_list = oid_concat.sort_values('OID').index.tolist()
-unique_list = list( map(int, unique_list) )
-missing = []
-for i in range(1,max(unique_list)):
-    if i not in unique_list:
-        missing.append(i)
-
-# TODO strange, OID 1 is missing, many other values between 3926 - 14711
-# no apparent pattern to whats missing, total 347 missing elts
-# did some poking around at the missing features in QGIS, seems like they are values
-# where the road was monitored multiple times in one year (ex. OID 14711 and 14712)
-# were both monitored in 2013, and only the value 14712 comes through in the rasterized
-# OID roads created with vector2raster.py
-# also checked with OID 3926, same deal (overlapped with OID 6868, both in year 2011)
-# TODO so... does this mean that the better approach is not ever converting to raster? 
-# or some workaround while still using rasters?
-
-datatif = '/home/myles/middsense/data_from_drive/SAR_Amplitude/20110829_110031_20110829_110037_CSKS3-H4-0B_HH_SLC_data.tif'
-masktif = '/home/myles/middsense/OID Road Rasters/Staunton_OID_2011_IS.tif'
-
-def raster_group_means(datatif, masktif, dsttif):
+def raster2table(datatif, masktif):
     """
-    datatif: the raster whose values we want (SAR amplitude raster)
-    masktif: the raster used as a spatial mask (OID raster)
-    dsttif: output
+    opens the rasters using GDAL, converts to numpy arrays, extracts the 
+    unmasked data pixels, and returns a DataFrame with these values 
+
+    datatif: path to the raster whose values we want (SAR amplitude raster)
+    masktif: path to the raster used as a spatial mask (OID raster)
     """
-    #datatype = gdal.GDT_Float32
-    #drv = gdal.GetDriverByName('GTiff')
 
     # read files
     data = gdal.Open(datatif, gdal.GA_ReadOnly)
@@ -71,92 +47,76 @@ def raster_group_means(datatif, masktif, dsttif):
     # create an OID and amplitude stack with only the pixels where OID!=1 (the nodata value)
     # since we are filtering pixels, one dimension is lost, meaning that the
     # notion of location is only given by OID, not index in the array
-    # TODO also need to mask out the SAR nodata values which are skewing averages (-9999) 
     # TODO OID 1 is getting masked out, need to change nodata value in the OID rasters
-    stack = np.stack((mask_array[mask_array != 1], data_array[mask_array != 1]), axis = 1)
-    df = pd.DataFrame(stack, columns = ['OID', 'amp_' + str(datatif)])
-    means = df.groupby(by = 'OID').mean()
-
-    return(means)
+    not_nodata = np.logical_and(mask_array != -9999, data_array != -9999) 
+    # stack = np.stack((mask_array[mask_array != 1], data_array[mask_array != 1]), axis = 1)
+    stack = np.stack((mask_array[not_nodata], data_array[not_nodata]), axis = 1)
     
-    # create an OID and amplitude stack with all the pixels in the extent
-    # stack_all = np.stack((mask_array, data_array), axis = 2)
-
-
-    # df[0].unique() # list of OIDs
-    # np.unique(mask_array[mask_array != 1]) # alternatively, via numpy
-    #dst_array = (mask_array!=1) * data_array
-    #mask_array_01 = mask_array != 1
-    #dst_array = data_array[mask_array_01]
-    #mask_array_clip = mask_array[mask_array_01]
+    # convert to a DataFrame, subsetting the data filename to extract only the
+    # date the image was taken, which is unique for each
+    df = pd.DataFrame(stack, columns = ['OID', 'amp_' + str(datatif[-59:-51])]) 
     
-    #stack = np.stack((dst_array, mask_array_clip), axis =1)
-    #stack = np.stack((mask_array, data_array), axis = 2)
-    #stack2 = stack[(stack!=1).nonzero()[:2]]
-    #np.savetxt("bar2.csv", stack, delimiter = ",")
-    #print((dst_array[0,0]))
-    #numpy.savetxt("foo.csv", dst_array, delimiter = ",")
-
-    # TODO data and mask should have the same resolution, alignment, etc.
-    # could add a check before proceeding
-
-    # create output layer
-    # dst = drv.Create(dsttif, data.RasterXSize, data.RasterYSize, 1, datatype, options=['COMPRESS=DEFLATE'])
-    # dst.SetProjection(data.GetProjectionRef())
-    # dst.SetGeoTransform(data.GetGeoTransform())
-        
-    # write the out file
-    #dst_band = dst.GetRasterBand(1)
-    #dst_band.SetNoDataValue(-9999)
-    #BandWriteArray(dst_band, dst_array)
-
     # close datasets
-    #data_band = None
-    #mask_band = None
+    data_array = None
+    mask_array = None
     data = None
     mask = None
-    #dst_band = None
-    #dst = None
-    
+
+    return df
+
+def summarize(df, method): 
+    """
+    groups dataframe 'df' by 'OID' column and summarizes by method 'method'
+    """
+    grouped = df.groupby(by = 'OID')
+    #return grouped
+    if method == 'mean':
+        summarized = grouped.mean()
+    elif method == 'count':
+        summarized = grouped.count()
+    elif method == 'median':
+        summarized = grouped.median()
+        
+    return summarized
+
 
 if __name__ == "__main__":
-    DESCRIPTION = "placeholder"
+    
+    start_time = time.time()
 
+    DESCRIPTION = "summarize data raster for each mask value"
+    
     parser = argparse.ArgumentParser(description=DESCRIPTION)
 
-    #parser.add_argument("datatif")
-    #parser.add_argument("masktif")
-    #parser.add_argument("dsttif")
-
-    parser.add_argument('-m', '--mask_tifs', nargs = '*')
-    #parser.add_argument("mask_tif", nargs=n_)
-    parser.add_argument('-d', '--data_tifs', nargs='*')
-    
-
+    parser.add_argument('-m', '--mask_tifs', nargs = '+',
+                        help = "path to mask .tif (required)")
+    parser.add_argument('-d', '--data_tifs', nargs = '+',
+                        help = "path to data .tif (required)")
+    parser.add_argument('-o', '--out_path',
+                        help = "path to output csv (required)")
+    parser.add_argument('-s', '--summarize',
+                        help = "summarize method: from {'mean', 'median', 'count', 'none'}")
     args = parser.parse_args()
 
-    print("masks: " + str(args.mask_tifs))
-    print("data: " + str(args.data_tifs))
+    n = len(args.mask_tifs) * len(args.data_tifs)
+    proceed = input("run on all " + str(n) + " (data, mask) pairs? (y/n): ")
 
-    print("masks: ")
-    for mask in args.mask_tifs:
-        print(mask)
-    
-    print("data: ")
-    for data in args.data_tifs:
-        print(data)
+    if proceed == 'y':
+        temp0 = []
+        progress = 0
+        for data in args.data_tifs:
+            temp1 = []
+            for mask in args.mask_tifs:
+                temp1.append(summarize(raster2table(data, mask), args.summarize))
+                progress += 1
+                print("calculation completed for " + str(progress) + "/" + str(n) + " file pairs") 
+            temp0.append(pd.concat(temp1, axis = 0))
 
-    #OIDs = []
-    #for i in range(5):
-        #print(len(raster_group_means(args.data_tifs[0], args.mask_tifs[i], '')))
-        #OIDs.append((raster_group_means(args.data_tifs[0], args.mask_tifs[i], '').index))
-    
-    #OIDs_array = np.array(OIDs)
-    #np.savetxt("bar3.csv", OIDs_array, delimiter = ",")
+        out = pd.concat(temp0, axis = 1)    
+        out.to_csv(path_or_buf = args.out_path)
 
-    # TODO all the means tables together have 15930 rows, less than the 
-    # 16277 range(1,16277) features in Staunton_Maint_All_Buf12_10_10.dbf
-    # not sure what is being left out
-
-    
-    #raster_group_means(args.datatif, args.masktif, args.dsttif)
+        print("execution time (seconds): " + str(time.time() - start_time))
+        print("for {} data rasters, {} mask rasters".format( \
+            len(args.data_tifs), len(args.mask_tifs)))
+    else: 
+        print("exiting")
