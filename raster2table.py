1033aaec80176a67117707ac04739d7af0c082f5
mylesstokowski <mylesstokowski@gmail.com>
2020-07-04 10:07:53 -0400

    moved multiple image functionality to merge()

--- raster2table.py
+++ raster2table.py
@@ -44,16 +44,14 @@ def raster2table(datatif, masktif):
     data_array = BandReadAsArray(data.GetRasterBand(1))
     mask_array = BandReadAsArray(mask.GetRasterBand(1))
     
-    # create an OID and amplitude stack with only the pixels where OID!=1 (the nodata value)
+    # create an OID and amplitude stack, filtering out nodata values
     # since we are filtering pixels, one dimension is lost, meaning that the
     # notion of location is only given by OID, not index in the array
-    # TODO OID 1 is getting masked out, need to change nodata value in the OID rasters
     not_nodata = np.logical_and(mask_array != -9999, data_array != -9999) 
-    # stack = np.stack((mask_array[mask_array != 1], data_array[mask_array != 1]), axis = 1)
     stack = np.stack((mask_array[not_nodata], data_array[not_nodata]), axis = 1)
     
     # convert to a DataFrame, subsetting the data filename to extract only the
-    # date the image was taken, which is unique for each
+    # image acquisition date, which is unique for each
     df = pd.DataFrame(stack, columns = ['OID', 'amp_' + str(datatif[-59:-51])]) 
     
     # close datasets
@@ -69,20 +67,44 @@ def summarize(df, method):
     groups dataframe 'df' by 'OID' column and summarizes by method 'method'
     """
     grouped = df.groupby(by = 'OID')
-    #return grouped
+
     if method == 'mean':
         summarized = grouped.mean()
     elif method == 'count':
         summarized = grouped.count()
     elif method == 'median':
         summarized = grouped.median()
+    else:
+        print('invalid or no method chosen')
         
     return summarized
 
+def merge(mask_tifs, data_tifs, out_path, method):
+    """
+    perform the calculation for all the (data, mask) pairs
+    and merge them together into one csv
+    start_time = time.time()
+    """
+    temp0 = []
+    n = len(mask_tifs) * len(data_tifs)
+    progress = 0
+
+    for data in data_tifs:
+        temp1 = []
+        for mask in mask_tifs:
+            temp1.append(summarize(raster2table(data, mask), method))
+            progress += 1
+            print("calculation completed for " + str(progress) + "/" + str(n) + " file pairs") 
+        temp0.append(pd.concat(temp1, axis = 0))
+    out = pd.concat(temp0, axis = 1)    
+
+    out.to_csv(path_or_buf = out_path)
+
+    print("execution time (seconds): {} \nfor {} data rasters, {} mask rasters".format( \
+        str(time.time() - start_time), len(data_tifs), len(mask_tifs)))
+
 
 if __name__ == "__main__":
-    
-    start_time = time.time()
 
     DESCRIPTION = "summarize data raster for each mask value"
     
@@ -94,35 +116,19 @@ if __name__ == "__main__":
                         help = "path to data .tif (required)")
     parser.add_argument('-o', '--out_path',
                         help = "path to output csv (required)")
-    parser.add_argument('-s', '--summarize',
+    parser.add_argument('-s', '--summarize_method',
                         help = "summarize method: from {'mean', 'median', 'count', 'none'}")
     parser.add_argument('-i', '--interactive', action = 'store_true',
                         help = "flag to pause to confirm calculation")
     args = parser.parse_args()
 
     n = len(args.mask_tifs) * len(args.data_tifs)
-
+    
     if args.interactive:
         proceed = input("run on all " + str(n) + " (data, mask) pairs? (y/n): ")
-    else:
+        
+    if (not args.interactive) or (proceed == 'y'):
         print("calculating summaries on all " + str(n) + " (data, mask) pairs")
-
-    if (not args.interactive) or proceed == 'y':
-        temp0 = []
-        progress = 0
-        for data in args.data_tifs:
-            temp1 = []
-            for mask in args.mask_tifs:
-                temp1.append(summarize(raster2table(data, mask), args.summarize))
-                progress += 1
-                print("calculation completed for " + str(progress) + "/" + str(n) + " file pairs") 
-            temp0.append(pd.concat(temp1, axis = 0))
-
-        out = pd.concat(temp0, axis = 1)    
-        out.to_csv(path_or_buf = args.out_path)
-
-        print("execution time (seconds): " + str(time.time() - start_time))
-        print("for {} data rasters, {} mask rasters".format( \
-            len(args.data_tifs), len(args.mask_tifs)))
+        merge(args.mask_tifs, args.data_tifs, args.out_path, args.summarize_method, n)
     else: 
         print("exiting")
