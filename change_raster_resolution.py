4ab6423489088d4ea200bbaf09b439ab20e0eff7
mylesstokowski <mylesstokowski@gmail.com>
2020-07-05 17:55:20 -0400

    minor cleaning

--- change_raster_resolution.py
+++ change_raster_resolution.py
@@ -1,32 +1,37 @@
-# Myles Stokowski
-# 6.19.2020
-# 
-# outputs new .tif with data from input .tif and projection/alignment from
-# reference .tif
-# 
-# conceived use case is converting 1m VA landcover layer to 3m 
-# resolution and same extent/alignment as SAR data
+"""
+Myles Stokowski
+6.19.2020
 
+outputs new .tif with data from input .tif and projection/alignment from
+reference .tif
+
+conceived use case is converting 1m VA landcover layer to 3m
+resolution and same extent/alignment as SAR data
+"""
 import sys
 import argparse
-try: 
+try:
     from osgeo import gdal
 except ImportError:
     sys.exit('ERROR: cannot find GDAL/OGR modules')
 
-def convert_resolution(srctif, dsttif, reftif): 
+def convert_resolution(srctif, dsttif, reftif):
+    """
+    output dsttif raster with data from srctif, resolution/extent from reftif
+    """
     datatype = gdal.GDT_Byte
     drv = gdal.GetDriverByName('GTiff')
-    
+
     # read files
     ref = gdal.Open(reftif, gdal.GA_ReadOnly)
     src = gdal.Open(srctif, gdal.GA_ReadOnly)
 
     # create output layer
-    dst = drv.Create(dsttif, ref.RasterXSize, ref.RasterYSize, 1, datatype, options=['COMPRESS=DEFLATE'])
+    dst = drv.Create(dsttif, ref.RasterXSize, ref.RasterYSize, 1, datatype, \
+        options=['COMPRESS=DEFLATE'])
     dst.SetProjection(ref.GetProjectionRef())
     dst.SetGeoTransform(ref.GetGeoTransform())
-    
+
     src_proj = src.GetProjection()
     ref_proj = ref.GetProjection()
 
@@ -48,17 +53,14 @@ if __name__ == "__main__":
 
     parser.add_argument("srctif",
                         help="path to source .tif containing data (required)")
-    
+
     parser.add_argument("dsttif",
                         help="destination path to output .tif (required)")
 
     parser.add_argument("reftif",
                         help="path to reference .tif containing target \
                                 resolution (required)")
-    
+
     args = parser.parse_args()
 
     convert_resolution(args.srctif, args.dsttif, args.reftif)
-
-
-
