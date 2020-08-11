05a591f98f6e38a0fa14bc8a6e756b47c8231805
mylesstokowski <mylesstokowski@gmail.com>
2020-08-11 14:52:59 -0400

    integrated pixel2road into automate_anlaysis

--- automate_analysis.py
+++ automate_analysis.py
@@ -287,12 +287,11 @@ def gen_all_sparse():
         save_sparse(sparse_roads, SPARSE_DIR+'roads/')
 
 # Generate and save all sparse matrices: raw, despeck, and roads
-# gen_all_sparse()
+gen_all_sparse()
 
-# TODO could load into memory before looping, low priority
 
 """
-Outputting DataFrames at the pixel level rather than at the road level
+Output DataFrames at the pixel level rather than at the road level
 """
 if os.path.exists(PIXEL_DIR):
     print('skipping pixelwise .pkl creation')
@@ -305,108 +304,46 @@ else:
         img_rd_stack.to_pickle(PIXEL_DIR + img_dir.split('/')[-2] + '_pixels.pkl')
 
 """
-Summarizing!
+Calculate metrics on each group of pixels within a road segment
+Pixel level data --> aggregated road level data
 """
 # for each of the SAR datasets (raw/despeck)
-for sar in glob(PIXEL_DIR + '*'):
+for pixel_path in glob(PIXEL_DIR + '*'):
 
-    df = pd.read_pickle(sar)
+    df = pd.read_pickle(pixel_path)
+    
+    # select only the SAR amplitude columns
     sar_cols = df.iloc[:,-67:]
 
     # for each of the road datasets (buffered/not buffered, masked/unmasked)
-    for roads in ['centerline_unmasked', 'centerline_masked', 'buffered_masked', 'buffered_unmasked']:
+    for road_type in ['centerline_unmasked', 'centerline_masked', 'buffered_masked', 'buffered_unmasked']:
 
-        oid_cols = df.filter(like=roads)
+        # select only the OID columns for given road dataset
+        oid_cols = df.filter(like=road_type)
+        
         concat_list = []
 
         for year in oid_cols:
+                
+            year_df = pd.concat([oid_cols[year].rename('oid'), sar_cols], axis=1).set_index('oid')
+            concat_list.append(year_df.loc[year_df.index.isna() == False])
 
-            year_df = pd.concat([oid_cols[year].rename('oid'), sar_cols], axis=1)
-            concat_list.append(year_df.loc[year_df['oid'].isna() == False])
+        # concat the rows for each year of data, and 
+        # sort by OID (axis=0) and SAR acquisition date (axis=1)
+        all_oids = pd.concat(concat_list, axis=0).sort_index(axis=0).sort_index(axis=1)
 
-        all_oids = pd.concat(concat_list, axis=0)
-
-        pixel2road.all_metrics(all_oids)
-
-# sar_type = ['raw/', 'despeckled/']
-# buffer_type = ['All_Buf12_10_10_within_footprint', 'All_within_footprint']
-# mask_type = ['_landcovermasked.npz', '.npz']
-
-# # list of possible combinations of data sources (raw/despeck, buffered/not buffered, masked/unmasked)
-# datagroups = np.array(np.meshgrid(sar_type, buffer_type, mask_type)).T.reshape(-1, 3)
-# names = np.array(np.meshgrid(['raw', 'despeck'], ['buffered', 'centerline'], ['masked', 'unmasked'])).T.reshape(-1, 3)
-
-
-# print('Summarizing...')
-# # 31 Generates a .csv file for each combination of data inputs
-# for i, dg in enumerate(datagroups):
-
-#     out_path = str('{}{}_{}_{}.csv'.format(CSV_DIR, names[i][0], names[i][1], names[i][2]))
-
-#     # check if this summary .csv already exists
-#     if os.path.exists(out_path):
-#         print('skipping summary of {}, file already exists'.format(out_path))
-
-#     else:
-#         print('converting ' + ROAD_GDB + ' to shapefiles and csv')
-
-#         # For each .csv, we summarize each image over the road files for each year:
-#         summarylist = []
-
-#         # Each image in selected stack (raw/despeck)
-#         for img_path in glob(SPARSE_DIR + dg[0] + '*'):
-
-#             img = sparse.load_npz(img_path)
-
-#             concat_list = []
-
-#             # Each year of road segments matching the road type we want
-#             for rd_path in glob(SPARSE_DIR + 'roads/*' + dg[1] + dg[2]):
-
-#                 rd = sparse.load_npz(rd_path)
-
-#                 # mask SAR image to only pixels where there was an OID road segment that year
-#                 img_masked = img.multiply(rd > 0).tocoo()
-#                 rd_masked = rd.multiply((img_masked > 0) + (img_masked == -1)).tocoo() # addition works as logical OR
-
-#                 # Check that the masked files are the same size
-#                 if img_masked.size != rd_masked.size:
-#                     print('error, sparse matrix sizes dont match!!!')
-
-#                 # Convert to DataFrame
-#                 sar_col_name = img_path.split('/')[-1][0:8] + '_'
-
-
-#                 pixels = pd.DataFrame({
-#                     'oid':rd_masked.data,
-#                     'amp': img_masked.data
-#                 })
-
-                pixels = pixels.replace(-1, 0)
-
-                concat_list.append(pixels)
-
-
-            # concatenate year lists for this image
-            all_oids = pd.concat(concat_list, axis=0)
-
-            # Summarize !!!!
-            summarized = pixel2road.all_metrics(all_oids)
-            summarized.columns = sar_col_name + summarized.columns
-
-            # list of dataframes by image
-            summarylist.append(summarized)
-
-
-        fullsummary = pd.concat(summarylist, axis=1)
-        fullsummary.index.name = 'oid'
-
-        fullsummary.to_csv(out_path)
+        summarized = pixel2road.all_metrics(all_oids)
+        
+        out_path = pixel_path.split('/')[-1][:-11] + '_' + road_type + '.pkl'
+        pd.to_pickle(summarized, out_path)
 
         print('summarized ' + out_path)
 
 """
-Merging and cleaning the datasets
+Road level data merging, cleaning and calculating additional metrics/features
+- merge SAR data with IRI data (and other road quality metrics)
+- mask/remove bad data with zero-value pixels, etc.
+- calculate additional metrics like rate of change in mean amplitude across images
 """
 
 if os.path.exists(PKL_DIR):
--- pixel2road.py
+++ pixel2road.py
@@ -26,27 +26,24 @@ def quantiles(grouped):
   iqr_prod = 1.5 * iqr
   min = q1 - iqr_prod
   max = q3 + iqr_prod
-
+  
+  # rename columns
   q1, q3, iqr = q1.add_suffix('_q1'), q3.add_suffix('_q3'), iqr.add_suffix('_iqr')
   min, max = min.add_suffix('_bmin'), max.add_suffix('_bmax')
 
-  # concatenate
-  out = pd.concat([q1, q3, iqr, min, max], axis=1)
-
-  return out
+  return pd.concat([q1, q3, iqr, min, max], axis=1)
 
 def all_metrics(df):
   """
   compute all summaries
   """
-
+  
   # group by OID
   grouped = df.groupby('oid', as_index=True)
 
-  # built in pandas functions
-  # count
+  # count pixels per road segment
   count = grouped.count().add_suffix('_count')
-  zerocount = df.set_index('oid').eq(0).groupby('oid').sum().add_suffix('_zerocount')
+  zerocount = df.eq(0).groupby('oid').sum().add_suffix('_zerocount')
 
   # mean, median, stddev, min, max (not boxplot min/max)
   mean = grouped.mean().add_suffix('_mean')
@@ -55,20 +52,35 @@ def all_metrics(df):
   min = grouped.min().add_suffix('_min')
   max = grouped.max().add_suffix('_max')
 
-  # merge = pd.concat([mean, median, count, zero, quant, std, min, max], axis=1, join='outer')
-  merge = pd.concat([mean, median, count, std, min, max], axis=1, join='outer')
+  # combine the statistics series --> dataframe 
+  merge = pd.concat([mean, median, count, zerocount, std, min, max], axis=1, join='outer')
   merge = merge.fillna(0) # fill 0s for zero_count column
 
   # quantiles
   quant = quantiles(grouped)
 
-  # Mean filtering
+  # pixel filtering based on boxplot min/max
   maxs = quant.filter(like='bmax')
-  mins = quant.filter(like='bmins')
+  mins = quant.filter(like='bmin')
+  maxs.columns = (maxs.columns.str.rstrip('_bmax'))
+  mins.columns = (mins.columns.str.rstrip('_bmin'))
 
-  merge = pd.concat([merge, quant, filtered_mean], axis=1, join='outer')
+  mask = df.lt(maxs) & df.gt(mins)
+  
+  # quick check if axes are same for mask and data DataFrames
+  if not np.unique(df.index == mask.index) and np.unique(df.columns == mask.columns):
+    print('error! pixel dataframe and boxplot filtering mask dataframe do not align')
+
+  # apply mask, calculate stats on filtered pixels
+  filtered = df[mask]  
+  filtered_mean = filtered.groupby('oid').mean().add_suffix('_filtered_mean')
+  filtered_std = filtered.groupby('oid').std().add_suffix('_filtered_std')
+  
+  # add the filtered stats and 0.25 and 0.75 quantiles
+  merge = pd.concat([merge, quant.filter(like='q1'), quant.filter(like='q3'), \
+                     filtered_mean, filtered_std], axis=1, join='outer')
 
   # sort columns
-  merge = merge.reindex(sorted(merge.columns), axis=1)
+  merge = merge.sort_index(axis=1)
 
   return merge
